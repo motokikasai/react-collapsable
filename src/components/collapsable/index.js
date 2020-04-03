ea34a137ec0c790dc11ca01eb638fcfd0da9e271
motokikasai <kmomoky@hotmail.com>
2020-04-03 23:20:15 +0200

    Apply simply layout & caret

--- src/components/collapsable/index.js
+++ src/components/collapsable/index.js
@@ -7,8 +7,12 @@ class Collapsable extends React.Component {
 
   render() {
     return (
-      <li>
-        <div key={this.props.data.id}>{this.props.data.question}</div>
+      <li key={this.props.data.id}>
+        <div className="header">
+          <span className="label">{this.props.data.question}</span>
+          <span className="caret">&#94;</span>
+        </div>
+        <div className="content">{this.props.data.answer}</div>
       </li>
     );
   }
